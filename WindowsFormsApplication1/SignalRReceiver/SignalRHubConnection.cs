622e0712deb54baea1d598a7d193d938dcc8b990
Patrick Laplante <laplante.patrick@gmail.com>
2016-10-13 14:26:56 -0400

    wip

---
+++ WindowsFormsApplication1/SignalRReceiver/SignalRHubConnection.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Threading;
+
+namespace SignalRReceiver
+{
+    public class SignalRHubConnection
+    {
+        HubConnection _connection = null;
+        IHubProxy _myHub;
+        public event EventHandler<string> MessageReceivedEvent;
+
+        public SignalRHubConnection()
+        {
+        }
+
+        public async void Connect()
+        {
+                _connection = new HubConnection("http://localhost:8080/");
+                _myHub = _connection.CreateHubProxy("MyHub");
+
+                _connection.Start().ContinueWith(task => {
+                    if (task.IsFaulted)
+                    {
+                        MessageReceivedEvent(this, "error connection");
+
+                        Console.WriteLine("There was an error opening the connection:{0}",
+                                          task.Exception.GetBaseException());
+                    }
+                    else
+                    {
+                        MessageReceivedEvent(this, "Connected");
+
+                        Console.WriteLine("Connected");
+                    }
+
+                }).Wait();
+
+                _myHub.On<string>("addMessage", param =>
+                {
+                    Console.WriteLine(param);
+                    MessageReceivedEvent(this, param);
+                });
+        }
+
+        public void SendMessage(string message)
+        {
+            _myHub.Invoke<string>("Send", message).ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error calling send: {0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine(task.Result);
+                }
+            });
+        }
+
+        public void Disconnect()
+        {
+            _connection.Stop();
+
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using SignalRReceiver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    /// <summary>
+    /// This is for the Thread way pre C#5 and .Net 4.5
+    /// </summary>
+    public static class ControlExtension
+    {
+        private delegate void SetPropertyThreadSafeDelegate<TResult>(
+             Control @this,
+             Expression<Func<TResult>> property,
+             TResult value);
+
+        public static void SetPropertyThreadSafe<TResult>(
+            this Control @this,
+            Expression<Func<TResult>> property,
+            TResult value)
+        {
+            var propertyInfo = (property.Body as MemberExpression).Member
+                as PropertyInfo;
+
+            if (propertyInfo == null ||
+                !@this.GetType().IsSubclassOf(propertyInfo.ReflectedType) ||
+                @this.GetType().GetProperty(
+                    propertyInfo.Name,
+                    propertyInfo.PropertyType) == null)
+            {
+                throw new ArgumentException("The lambda expression 'property' must reference a valid property on this Control.");
+            }
+
+            if (@this.InvokeRequired)
+            {
+                @this.Invoke(new SetPropertyThreadSafeDelegate<TResult>
+                (SetPropertyThreadSafe),
+                new object[] { @this, property, value });
+            }
+            else
+            {
+                @this.GetType().InvokeMember(
+                    propertyInfo.Name,
+                    BindingFlags.SetProperty,
+                    null,
+                    @this,
+                    new object[] { value });
+            }
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        SignalRHubConnection _connection;
+        int _counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            _connection = new SignalRHubConnection();
+            _connection.MessageReceivedEvent += _connection_MessageReceivedEvent;
+            _connection.Connect();
+        }
+
+        private void _connection_MessageReceivedEvent(object sender, string message)
+        {
+            label1.SetPropertyThreadSafe(() => label1.Text, message);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _connection.SendMessage("Hello world: " + _counter++);
+        }
+    }
+}
+
+//// This is the Task way C# >= 5 and .Net >= 4.5
+//var progress = new Progress<string>(s => label1.Text = "using task: " + s);
+//await Task.Factory.StartNew(() =>
+//{
+//    // Perform a long running work...
+//    for (var i = 0; i < 10; i++)
+//    {
+//        ((IProgress<string>)progress).Report(i.ToString());
+//        Task.Delay(500).Wait();
+//    }
+
+//}, TaskCreationOptions.LongRunning);
+//label1.Text = "completed";
+
+//// This is the Thread way pre C# < 5 and .Net < 4.5
+//new Thread(() =>
+//{
+//    Thread.CurrentThread.IsBackground = true;
+
+//    for (var i = 0; i < 10; i++)
+//    {
+//        label1.SetPropertyThreadSafe(() => label1.Text, "using thread: " + i.ToString());
+//        Thread.Sleep(500);
+//    }
+
+//    /* run your code here */
+//    label1.SetPropertyThreadSafe(() => label1.Text, "completed");
+//}).Start();
